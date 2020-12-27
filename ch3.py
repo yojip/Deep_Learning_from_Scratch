af3380f9bc54fbed60810012bf664824f0b5f151
yojip <55662375+yojip@users.noreply.github.com>
2020-12-27 19:16:15 +0900

    Add files via upload

--- ch3.py
+++ ch3.py
@@ -13,24 +13,30 @@ def relu(x):
 def identity_function(x):
     return x
 
-X = np.array([1.0,0.5])
-W1 = np.array([[0.1, 0.3, 0.5], [0.2, 0.4, 0.6]])
-B1 = np.array([0.1, 0.2, 0.3])
+def init_network():
+    network = {}
+    network['W1'] = np.array([[0.1, 0.3, 0.5], [0.2, 0.4, 0.6]])
+    network['b1'] = np.array([0.1, 0.2, 0.3])
+    network['W2'] = np.array([[0.1, 0.4],[0.2, 0.5], [0.3, 0.6]])
+    network['b2'] = np.array([0.1, 0.2])
+    network['W3'] = np.array([[0.1, 0.3], [0.2, 0.4]])
+    network['b3'] = np.array([0.1, 0.2])
+    return network
 
-A1 = np.dot(X, W1)+B1
-Z1 = sigmoid(A1)
+def forward(network, x):
+    W1, W2, W3 = network['W1'], network['W2'], network['W3']
+    b1, b2, b3 = network['b1'], network['b2'], network['b3']
 
-W2 = np.array([[0.1, 0.4],[0.2, 0.5], [0.3, 0.6]])
-B2 = np.array([0.1, 0.2])
+    a1 = np.dot(x, W1)+b1
+    z1 = sigmoid(a1)
+    a2 = np.dot(z1, W2)+b2
+    z2 = sigmoid(a2)
+    a3 = np.dot(z2, W3)+b3
+    y = identity_function(a3)
 
-A2 = np.dot(Z1, W2)+B2
-Z2 = sigmoid(A2)
+    return y
 
-W3 = np.array([[0.1, 0.3], [0.2, 0.4]])
-B3 = np.array([0.1, 0.2])
-
-A3 = np.dot(Z2, W3)+B3
-Y = identity_function(A3)
-#print(Y)
-
-
+network = init_network()
+x = np.array([1.0, 0.5])
+y = forward(network,x)
+print(y)
